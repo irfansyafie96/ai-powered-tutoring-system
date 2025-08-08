977fcb564211af9a713c0caf825f9559002fbe2c
irfansyafie96 <136911643+irfansyafie96@users.noreply.github.com>
2025-08-08 22:52:39 +0800

    Update Cloudinary config to use CLOUDINARY_URL format

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -2,11 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 
-// Configure Cloudinary
+// Configure Cloudinary using the URL format
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloudinary_url: process.env.CLOUDINARY_URL,
 });
 
 // Configure storage
